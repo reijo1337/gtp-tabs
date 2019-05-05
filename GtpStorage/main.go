0a462edb1c0415ba30aa64b95bb4409c4e668011
Grigory Tantsevov <tantsevov@gmail.com>
2019-05-05 20:38:01 +0300

    removing global variables

--- GtpStorage/main.go
+++ GtpStorage/main.go
@@ -2,32 +2,7 @@ package main
 
 import (
 	"log"
-	"os"
 )
-
-// Инициализирует глобальные переменные на основе системных
-func init() {
-	DatabaseUserName = os.Getenv("DB_USERNAME")
-	if DatabaseUserName == "" {
-		DatabaseUserName = "postgres"
-	}
-	DatabasePassword = os.Getenv("DB_PASSWORD")
-	if DatabasePassword == "" {
-		DatabasePassword = "postgres"
-	}
-	DatabaseName = os.Getenv("DB_NAME")
-	if DatabaseName == "" {
-		DatabaseName = "storage"
-	}
-	ServerPort = os.Getenv("SERVER_PORT")
-	if ServerPort == "" {
-		ServerPort = "8081"
-	}
-	DatabaseHost = os.Getenv("DB_HOST")
-	if DatabaseHost == "" {
-		DatabaseHost = "127.0.0.1"
-	}
-}
 
 func main() {
 	log.SetFlags(log.LstdFlags)
