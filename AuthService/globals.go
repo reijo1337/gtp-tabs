cdcd5f4c5181311d25740cdd0b590f4abcfab2ba
Grigory Tantsevov <tantsevov@gmail.com>
2019-05-05 21:10:29 +0300

    Config + makefile auth service

--- AuthService/globals.go
+++ AuthService/globals.go
@@ -1,16 +1,6 @@
 package main
 
 var (
-	// DatabaseUserName имя пользователя БД
-	DatabaseUserName string
-	// DatabasePassword пароль пользователя БД
-	DatabasePassword string
-	// DatabaseName имя БД
-	DatabaseName string
-	// ServerPort порт, на котором должен крутитсья сервис
-	ServerPort string
-	// DatabaseHost адрес бд
-	DatabaseHost string
 	Iss          = "authService"
 	Aud          = []string{"client1", "client2"}
 )
--- AuthService/main.go
+++ AuthService/main.go
@@ -2,39 +2,18 @@ package main
 
 import (
 	"log"
-	"os"
 )
-
-// Инициализирует глобальные переменные на основе системных
-func init() {
-	DatabaseUserName = os.Getenv("DB_USERNAME")
-	if DatabaseUserName == "" {
-		DatabaseUserName = "postgres"
-	}
-	DatabasePassword = os.Getenv("DB_PASSWORD")
-	if DatabasePassword == "" {
-		DatabasePassword = "postgres"
-	}
-	DatabaseName = os.Getenv("DB_NAME")
-	if DatabaseName == "" {
-		DatabaseName = "auth"
-	}
-	ServerPort = os.Getenv("SERVER_PORT")
-	if ServerPort == "" {
-		ServerPort = "8082"
-	}
-	DatabaseHost = os.Getenv("DB_HOST")
-	if DatabaseHost == "" {
-		DatabaseHost = "127.0.0.1"
-	}
-}
 
 func main() {
 	log.SetFlags(log.LstdFlags)
+	config, err := parseConfig("STORAGE")
+	if err != nil {
+		log.Panicln("Can't read config:", err)
+	}
 	r, err := SetUpRouter()
 	if err != nil {
 		log.Panicln("Can't set up server:", err)
 	}
-	log.Println("Starting server on port ", ServerPort)
-	r.Run(":" + ServerPort)
+	log.Println("Starting server on port ", config.Port)
+	r.Run(":" + config.Port)
 }
